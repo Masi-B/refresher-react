8e7815cd830084e17baf47b9ce8f4ece347ae406
Masimba <masimba.banda@gmail.com>
2021-09-25 17:41:02 +1000

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter';
 import Greet from './components/Greet';
 import Message from './components/Message';
 import Welcome from './components/Welcome';
@@ -7,6 +8,8 @@ import Welcome from './components/Welcome';
 function App() {
   return (
     <div className="App">
+      <Counter />
+      {/*
       <Greet name="Masimba" typename="firstName">
         <button>children props</button>
       </Greet>
@@ -14,7 +17,7 @@ function App() {
       <Greet name="Banda" typename="surname" />
       <Welcome name="David" typename="middle" />
       <Welcome name="Banda" typename="surname" />
-      <Message/>
+      <Message/> */}
     </div>
   );
 }
